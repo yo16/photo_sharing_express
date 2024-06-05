ecf0a568163c0c9a54beb469316431e5b32d87bd
yo16 <xxyo1xx@gmail.com>
2024-06-05 13:44:00 +0900

    redirect

--- app-photo-sharing/src/index.ts
+++ app-photo-sharing/src/index.ts
@@ -9,7 +9,16 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Multerの設定
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
 // トップ
 app.get('/', (req: Request, res: Response) => {
@@ -20,7 +29,9 @@ app.get('/', (req: Request, res: Response) => {
 // ファイルアップロード
 app.post('/upload', upload.single('file'), (req: Request, res: Response) => {
     if (req.file) {
-        res.send(`File uploaded: ${req.file.filename}`);
+        //res.send(`File uploaded: ${req.file.filename}`);
+        console.log(`File uploaded: ${req.file.filename}`);
+        res.redirect('/');
     } else {
         res.send('File upload failed.');
     }
