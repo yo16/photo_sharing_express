054639885719df1319da653ec5e4c660cbe55317
yo16 <xxyo1xx@gmail.com>
2024-06-06 10:30:54 +0900

    環境変数を利用

--- app-photo-sharing/src/index.ts
+++ app-photo-sharing/src/index.ts
@@ -5,10 +5,12 @@ import fs from 'fs';
 import { google } from 'googleapis';
 import { authorize } from './googleAuth';
 import { PassThrough } from 'stream';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
-const FOLDER_ID = "1HLOy0NptbpJ77MqLdRTnfMLBfvTPNaFK";
 
 
 // テンプレートエンジンの設定
@@ -19,21 +21,40 @@ app.set('views', path.join(__dirname, '../views'));
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+// ユーザー数
+const strUserCount: string|undefined = process.env.USER_COUNT;
+const userCount: number = (strUserCount)? Number(strUserCount): 4;
+
+// ユーザー名（カンマ区切りで名前を指定）
+const strUserNames: string|undefined = process.env.USER_NAMES;
+const userNames: string[] = (strUserNames)? strUserNames.split(','): [];
+
+
 // トップ
 app.get('/', (req: Request, res: Response) => {
     const message = req.query.m as string;
     const user = req.query.u as string;
 
     //const dynamicValue = "This is a dynamic value!";
-    res.render('upload', { dynamicValue: message, user });
+    res.render('upload', { dynamicValue: message, currentUser: user, userCount, userNames });
 });
 
 
 // ファイルアップロード
 app.post('/upload', upload.any(), (req: Request, res: Response) => {
-    const uploader: string = (req.body.optUser)
-        ? `user${req.body.optUser}`
-        : "unknown";
+    function getUserName(optUser: string|undefined, userNames: string[]) {
+        if (!optUser) {
+            return "unknown";
+        }
+
+        const numOptUser = Number(optUser);
+        if (numOptUser < userNames.length) {
+            return userNames[numOptUser];
+        }
+
+        return `user${numOptUser+1}`;
+    }
+    const uploader: string = getUserName(req.body.optUser, userNames);
 
     if (req.files) {
         const files = req.files as Express.Multer.File[];
@@ -63,9 +84,10 @@ app.listen(port, () => {
 // Google Driveにファイルをアップロードする関数
 function uploadFileToDrive(auth: any, file: Express.Multer.File, uploader: string) {
     const drive = google.drive({ version: 'v3', auth });
+    const folderId = process.env.GOOGLE_DRIVE_FOLDER_ID? process.env.GOOGLE_DRIVE_FOLDER_ID: '';
     const fileMetadata = {
         name: `${uploader}_${file.originalname}`,
-        parents: [FOLDER_ID]
+        parents: [folderId]
     };
     const bufferStream = new PassThrough();
     bufferStream.end(file.buffer);
